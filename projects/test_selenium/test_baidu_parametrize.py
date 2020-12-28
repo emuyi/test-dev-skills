be678559e3907f1ca9e3a41646d0857b8a6aaa00
MUYI <1120769355@qq.com>
2020-12-28 23:44:52 +0800

    update

--- projects/test_selenium/test_baidu_parametrize.py
+++ projects/test_selenium/test_baidu_parametrize.py
@@ -1,8 +1,3 @@
-"""
-不能把 parameterized 简单的理解成把测试用例的数据做参数然后传给它，因为传参的形式测的是一个用例
-但传参的目的是要测试多种情况，理应是多个测试用例。
-参数化的百度搜索
-"""
 import pytest
 import time
 from selenium import webdriver
@@ -10,7 +5,6 @@ from selenium import webdriver
 data = ['selenium', 'pytest', 'parameterized']
 
 
-# !!!!!!!!!!!!!!!! 如果这里不加 scope 会怎么样？
 @pytest.fixture(scope='class')
 def driver():
     driver = webdriver.Chrome()
