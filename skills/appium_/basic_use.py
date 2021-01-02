f58ebf9f9c8a2f54afd914d8b2e47e90ddfa9468
MUYI <1120769355@qq.com>
2021-01-02 18:03:34 +0800

    appium part one

---
+++ skills/appium_/basic_use.py
@@ -0,0 +1,99 @@
+"""
+工作原理：
+    1、Appium 的工作原理和 selenium 很类似，不过是将 WebDriver 换成了 Appium Server，同样是
+    测试脚本(client) 向 Appium Server 发送请求，Server 接受到请求后解析请求并操作移动设备或者模拟器
+    然后将结果 response。
+    2、appium  API 是在支持 selenium API 基础上又进行了扩展，如 appium 的 command 直接 webdriver 的 Remote
+    Connection._commands 这个属性字典进行赋值扩展处理。
+
+基本用法：
+    一、环境
+         server端: appium-desktop（appium server 的图形化工具，类似 selenium-ide）
+            ！注意 server 端出现 No Android home 等环境变量问题时，可以去appium-desktop config 里面设置下
+         client端：就 python 而言，appium-python-client 包
+
+    二、使用
+    from appium import webdriver
+
+    # 配置信息 可参考：https://github.com/appium/appium/blob/master/docs/en/writing-running-appium/caps.md
+    desired_caps = {
+        'deviceName': 'Android Emulator',
+        'automationName': 'Appium',
+        'platformName': 'Android',
+        'platformVersion': '11',
+        'appPackage': 'com.google.android.apps.nexuslauncher',
+        'appActivity': 'com.google.android.apps.nexuslauncher.NexusLauncherActivity',
+        'noRest': True,
+    }
+    ! 注意可以使用 adb shell dumpsys activity | grep(findstr) mCurrentFocus 或
+    adb shell dumpsys window | grep(findstr) mCurrentFocus 来获取 appPackage 及 appActivity
+
+    driver = webdriver.Remote(command_executor='http://127.0.0.1:4723/wd/hub')   # 4723： appium server 默认端口号
+    driver.find_element_by_id('xxxxx').click()
+    driver.quit()
+
+常用 API：
+    一、定位控件
+        driver.find_element_by_id()  # resource-id 属性
+        driver.find_element_by_accessibility_id()   # Android 里相当于 content-desc 属性
+        # 根据 uiautomator2 语法来定位
+        # new UiSelector().text("Camera"); .description(); .resourceId();
+        driver.find_element_by_android_uiautomator('new UiSelector().text("Camera")')
+        driver.find_element_by_class_name()  # class 属性
+        driver.find_element_by_xpath()
+
+        至于 webdriver 的 find_element_by_name/ by_tag_name/ by_link_text 之类的在 web app 或者混合应用的 WebView（原生 app 中内嵌 web
+        网页）中可以使用
+
+        其他的API:
+            driver.find_element_by_ios_predicate()
+            driver.find_element_by_ios_uiautomation()
+            driver.find_element_by_ios_class_chain()
+
+    二、常用操作
+        1、 操作应用
+            driver.install_app(xx.apk)
+            driver.remove_app(packageName)
+
+            driver.close_app()  # 关闭应用
+            driver.launch_app()  # 启动应用 常和 close_app() 配合使用，因为默认情况下实例化driver的时候配置的应用就会被启动
+
+            driver.is_app_installed(packageName)
+            driver.background_app(5)  # 将应用置于后台5s
+            driver.reset()  # 重置应用，相当于清除应用缓存
+            driver.current_package/driver.current_activity
+
+        2、 上下文操作 （针对混合应用）
+            WebView 组件无法通过 uiautomatorviewer 进行定位，且属性一般包含 WebView
+            contexts = driver.contexts   # ['NATIVE_APP', 'WEBVIEW_xxx']
+            driver.switch_to.context('WEBVIEW_xxx')
+            current_context = driver.current_context
+            driver.switch_to.context('NATIVE_APP')
+
+        3、键盘操作（注意光标要聚焦到输入框）
+            driver.find_element_by_id('android:id/search_src_text').send_keys('display')
+            driver.keyevent(keycode) # http://developer.android.com/reference/android/view/KeyEvent.html
+            driver.hide_keyboard()  收起虚拟键盘
+
+        4、TouchAction （tap，long_press, move_to, wait).perform()
+
+            from appium.webdriver.common.touch_action import TouchAction
+            action = TouchAction(driver)
+
+            action.tap(widget=None, x=None, y=None).perform()
+            action.long_press(widget=None, x=None, y=None).perform()
+
+            # press 和 move 配合链式操作，实现移动控件以及多点触控的效果 !!!!
+            action \
+            .press(x=start_x, y=start_y) \
+            .wait(ms=duration) \              等价于 driver.swipe(startX, startY, endX, endY)
+            .move_to(x=end_x, y=end_y) \
+            .release()
+             action.perform()
+        5. 其他操作
+            width, height = driver.get_window_size()
+            driver.lock(5) 锁屏并息屏5s, 5s后自动解锁亮屏，和按锁屏键效果一样
+            driver.push_file(devicePath, localPath)       感觉不如 adb push 和 adb pull 好用
+            file_content = driver.pull_file(deviceFilePath)
+"""
+
