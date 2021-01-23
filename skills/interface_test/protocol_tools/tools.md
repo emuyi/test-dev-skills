1c13c7378d9644218197b3af9e6b5b07b04b8127
MUYI <1120769355@qq.com>
2021-01-23 21:48:27 +0800

    requests done

--- skills/interface_test/protocol_tools/tools.md
+++ skills/interface_test/protocol_tools/tools.md
@@ -37,11 +37,7 @@ tcpdump 可以根据一定的过滤机制来抓取想要的数据包，常使用
 charles 本质上是将自己做为系统网络的代理服务器，所有的网络请求和响应都要经过它，因此可以实现
 网络数据的截取和分析。
 比较有用的功能：
-	2、模拟弱网
-    	Proxy --> Throttle Settings 
-    3、在指定的端口设置代理
-    	Proxy --> Proxy Settings
-    4、抓取https请求
+     1、抓取https请求
     	web：https://blog.csdn.net/u011019468/article/details/107404990
     	安卓模拟器：6.0以下的版本接受用户级别的证书，以上的版本需要修改apk包的属性
     			  1.手机上设置代理，charles代理可以通过 help->ssl proxy->install * on mobile device 会弹框提示
@@ -49,11 +45,15 @@ charles 本质上是将自己做为系统网络的代理服务器，所有的网
     			  	【模拟器右侧工具栏 ... --> setting --> proxy】
     			  2. 手机浏览器访问 http://chls.pro/ssl 下载并安装证书
     	安卓模拟器搞不了，用雷电模拟器，低版本的夜神理论上也可以。
-	5、mock
+	2、模拟弱网
+    	Proxy --> Throttle Settings 
+    3、在指定的端口设置代理
+    	Proxy --> Proxy Settings
+	4、mock
 		tools--> rewrite(最常用)-->enable rewrite
 		tools--> map remote
 		tools--> map local
-	1、指定要录制(抓取)的网站
+	5、指定要录制(抓取)的网站
 		Proxy --> Recording Settings --> Include		
 https://www.charlesproxy.com/documentation/(首推)
 https://juejin.cn/post/6844903665304600589
@@ -81,7 +81,7 @@ fiddler 提供的功能和charles类似
 -o 将响应信息保存为文件，curl -L -s -o 'ret.html'  http://www.taobao.com
 -A 指定User-Agent，注意尽量用单引号。 curl -A 'emuyi' www.taobao.com
 -b 携带cookie, curl -b 'foo=bar;a=1' www.baidu.com 或从文件中读取cookie，和-c配合使用
--c 将服务器设置的cookie写入到文件 curl -b cookies.txt www.baidu.com -v
+-c 将服务器设置的cookie写入到文件 curl -c cookies.txt www.baidu.com -v
 -d 指定post请求发送的数据，默认数据类型是*urlencoded的，并且会自动将请求转换为post， -X POST可以忽略。
 $ curl -d'login=emma＆password=123' https://google.com/login
 # 或者
@@ -103,20 +103,19 @@ curl -d @data.txt https://google.com/login
 	https://linux.die.net/man/1/curl
 	http://www.ruanyifeng.com/blog/2019/09/curl-reference.html
 	https://catonmat.net/cookbooks/curl
-
--i 获取响应信息
--I 发送了一个 HEAD 请求获取响应头信息
 -k 跳过ssl检测
 -L curl默认不进行重定向，—L 会向浏览器一样进行自动定向
+-X 指定请求的方式
+-i 获取响应信息
+-I 发送了一个 HEAD 请求获取响应头信息
 -s silent
 -u 用来设置服务器认证的用户名和密码 curl -u 'bob:12345' https://google.com/login
--X 指定请求的方式
 ```
 
 ##### 2、postman
 
 ```shell
-如何设置ssl证书：https://www.cnblogs.com/hailongchen/p/9902838.html
+postman 如何设置ssl证书：https://www.cnblogs.com/hailongchen/p/9902838.html
 ```
 
 
--- skills/interface_test/requests_/basic_use.py
+++ skills/interface_test/requests_/basic_use.py
@@ -1,15 +1,52 @@
 """
-request框架的文档写的非常好，通读文档就对啦！
+requests 框架的文档写的非常好，通读文档就对啦！
 docs：https://requests.readthedocs.io/zh_CN/latest/
-帮助练习request的网站：https://httpbin.testing-studio.com/
+帮助练习requests 的网站：https://httpbin.testing-studio.com/
 1、快速上手
+    请求相关
+        - 请求方法：requests.get .post .put .delete .head
+        - get请求携带参数：params={key:value, key1:[value1, value2]}
+        - post请求发送数据：(data参数指定)
+                1、默认是发送的编码 *form-urlencoded数据, 即发送的是表单数据
+                2、处理checkbox, select 之类的标签数据呢？ data=((key, value1), (key, value2))
+                3、发送json数据，data=json.dumps(data_dict) or json=data_dict
+                4、上传文件，files={files:open(xx, rb)} 文件类型是 application/octest-stream **
+                   显示指定上传文件的类型，files={files:(file, open(), image/jpg)}
+        - headers参数 指定请求头
+        - timeout参数，指定连接超时的时间
+        - 除了 head 请求，默认自动重定向，即 allow_redirects 默认是 True的，如果要禁止重定向，allow_redirects=False即可
+          可以通过 r.history 获取访问历史
+    响应相关
+        - 状态码
+            r.status_code
+            r.raise_for_status 指 4xx， 5xx时候回抛出异常
+            此外，内部还维护了一个包含状态码和描述的 codes字典，requests.codes, 如通过requests.codes.ok 是否是 true来判断请求的是否成功
+        - 响应头
+            r.headers
+        - 响应体
+            r.text 解码处理后的文本 【如果解码失败，可以通过r.content 来看返回内容设置的编码，然后使用 r.encoding进行编码设置】
+            r.content 字节数据
+            r.json()
+            r.raw() 原始套接字数据
+    cookie相关
+            1、请求携带cookie， 指定cookies={name：value}
+            2、r.cookies 获取服务端设置的cookies，
+            3、r.cookies 是一个 RequestsCookieJar 对象，类似字典，但封装了更多的功能，比如跨域或者跨路径携带
+            cookie，本质是给cookie的domain,path 属性赋值
+              jar = requests.cookies.RequestsCookieJar()
+              jar.set('name', 'bobby', domain=host.split('//')[-1], path='/cookies')
 2、进阶用法
+
+3、身份认证
+
 """
 # ===================================快速上手=====================================================
 # https://requests.readthedocs.io/zh_CN/latest/user/quickstart.html
 # -------------------------------------请求相关----------------------------------------------------
 import requests
 from requests.cookies import RequestsCookieJar
+from requests.utils import add_dict_to_cookiejar
+from requests.auth import AuthBase
 
 chrome = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
 
@@ -59,7 +96,7 @@ print(r.text)
 # r.text 会自动解码，但是如果解码出现了问题，可以通过 r.content 来查看返回的html或xml(应该会有编码的指定)
 # 然后使用 r.encoding=xxx, 来指定编码
 # r.json() 服务端返回的数据是json的话，使用r.json(), 且也将json数据直接load成dict了
-# r.raw 获取原始的套接字响应，需要设置 stream=True 参数 通过 iter_content 来获取
+# r.raw 获取原始的套接字响应，需要设置 stream=True 参数 通过 iter_content 来获取 [r.raw]
 """
 with open(filename, 'wb') as fd:
     for chunk in r.iter_content(chunk_size):
@@ -102,17 +139,219 @@ jar.set('name', 'hanks', domain=host.split('//')[-1], path='/get')
 r = requests.get(host + '/cookies', cookies=jar)
 print(r.text)
 
-# 关于重定向
+# ------------------------------------重定向相关--------------------------------------------------
 # 1、除了 HEAD 请求，requests 默认是自动重定向的，原因是里因为控制是否重定向的参数 allow_redirects 默认是 True
-# 所以如果想禁止重定向，allow_redirects = False, 同理如果HEAD 请求想要自动重定向，allow_redirects = True
+# 所以如果想禁止重定向，allow_redirects = False, 同理如果HEAD 请求想要自动重定向，get请求的时候设置 allow_redirects = True
 # 2、r.history 重定向的历史，它返回的是 response 对象的列表，按照访问历史由老到新进行排序
 r = requests.get('http://www.taobao.com')
-print(r.status_code, r.text)
+print(r.status_code)
 # print(r.history[0].status_code)
 
-# timeout 参数，指定连接超时时间(注意timeout限制的不是响应体下载的时间，而是建立连接的时间)
+# ------------------------------------timeout参数--------------------------------------------------
+# ，指定连接超时时间(注意timeout限制的不是响应体下载的时间，而是建立连接的时间)
 # 如果没有设置 timeout 参数，比如要访问网站网络不通，它会夯住，直到底层的urllib3 抛错，如目标计算机拒绝访问之类的
 r = requests.get('https://www.baidu.com')
-
+print('*' * 200)
 # ===================================进阶用法=====================================================
 # https://requests.readthedocs.io/zh_CN/latest/user/advanced.html
+
+# ------------------------------------会话对象 Session()--------------------------------------------------
+# 会话对象 requests.Session() 跨请求的时候保持某些参数
+
+# 1.保持cookies (登录后能正常的访问其他页面而不必重新登录)
+s = requests.Session()
+r1 = s.get(host + '/cookies/set/freeform/aaa')
+r = s.get(host + '/cookies')
+print(r.text)
+# 2. 注意只有会话级别的cookie才能在会话区间进行保持，方法级别设置的cookie不会被保持
+r = s.get(host + '/cookies', cookies={'a': '1', 'freeform': 'bbb'})
+print(r.json())  # {'cookies': {'a': '1', 'freeform': 'bbb'}}
+r = s.get(host + '/cookies')
+print(r.json())  # {'cookies': {'freeform': 'aaa'}}
+# 3.会话界别设置cookie requests.utils.add_dict_to_cookiejar(cj, cookie_dict)
+add_dict_to_cookiejar(s.cookies, {'name': 'bobby'})
+# 4.给请求方法设置默认参数 如头部信息
+s.headers.update({'server': 'emuyi'})  # 设置了一个默认的头部信息，之后所有的会话发送的请求都会携带
+r = s.get(host + '/headers', headers={'server': 'bobby'})   # 但方法级别如果设置了同名的信息，方法级别会覆盖会话级别
+print(r.text)
+# 5、可以放在 with 语句块中做上下文管理 确保会话期间出现异常退出时，会话也能被关闭
+with requests.session() as s:
+    s.get('https://www.baidu.com')
+# 6、如果会话层的某些键的数据不想要，可以在方法层将该键的值设置为None
+
+# ------------------------------------响应对象包含请求对象--------------------------------------------------
+# requests.get() 做了两件事，1.创建一个request对象，去某个服务器上请求资源，2.服务器响应返回一个response对象，response对象
+# 中既包含响应的资源，也包含请求的对象request，如何去访问它，只需r.request 即可
+
+r = requests.get('https://www.baidu.com')
+print(r.request.headers.get('user-agent'))  # <PreparedRequest [GET]>
+
+# ------------------------------------PreparedRequest-------------------------------------------------------
+# https://requests.readthedocs.io/zh_CN/latest/user/advanced.html#prepared-request
+# requests发送的请求都是一个PreparedRequest对象，简单理解PreparedRequest 对象它是一个封装好了请求头，请求数据，cookies等数据
+# 的请求对象，是对request.get 等请求的偏底层的解释
+"""
+requests.get 的源码流程
+    无论是get请求还是 post请求，本质上都会走 session.request(method, url, data, headers...)
+    session.request 拿着这些参数实例化 req = Request(method, url, data, headers...) 对象
+    再去调用 prepare_request(req) 方法构造 PreparedRequest() 对象，最后走 session.send(prepped) 
+"""
+# 如以下示例：
+r = requests.get('https://www.baidu.com')
+# 用偏底层的写法来写的话
+s = requests.Session()
+req = requests.Request('GET', 'https://www.baidu.com')
+prepped = s.prepare_request(req)
+# prepped.prepare_body()
+# prepped.prepare_header()
+res = s.send(prepped)
+
+# ------------------------------------SSL 证书验证-------------------------------------------------------------
+# https://requests.readthedocs.io/zh_CN/latest/user/advanced.html#ssl
+# 像浏览器一样， requests默认是开启SSL认证的, 如果认证失败，可以把证书通过 verify 参数指定证书路径，或者放到 REQUESTS_CA_BUNDLE环境变量里
+# 或者将 verify 设置为 False
+# os.environ.get('REQUESTS_CA_BUNDLE')
+# os.environ.get('CURL_CA_BUNDLE')
+# 如果一个网站的根证书是受信任的，那么他的子证书都是可信任的，对于浏览器来说，它会通过证书链去找这个网站的根证书，然后确认是不是自己信任的根证书机构
+# 颁发的  “由于CA 根证书不在“受信任的根证书颁发机构”存储区中，所以它不受信任。” --- 出自 google
+# r = requests.get('https://inv-veri.chinatax.gov.cn/')
+
+# requests 内嵌了一套受信任的根证书，来源于 Mozilla trust store，这些证书只有在requests更新的时候才会被更新，所以要尽量保持requests时常
+# 更新，同时requests也会使用 certifi 库里的证书，也要尽量保持 certifi 的及时更新。
+
+# ------------------------------------流式下载(下载大文件)-------------------------------------------------------------
+# 默认情况下 requests 是立即下载响应体，文件比较的大的时候可以指定 stream=True 进行流式下载，
+# 当 stream = True的时候，requests会将延迟下载，这时可以使用iter_content(trunk_size), iter_lines(trunk_size)
+# 按照指定大小下载文件。
+
+# ! 需要注意的是，如果使用 stream=True 意味了着 Requests就没办法将连接释放回连接池，除非数据被全部接受完毕，或者显示调用 response.close()
+# 因此为了确保连接能够最终被释放，建议通过 with 语句来进行上下文的管理
+# with requests.get('https://github.com/kennethreitz/requests/tarball/master', stream=True) as r:
+#     with open('a.zip', 'wb') as f:
+#         for trunk in r.iter_lines(1024):
+#             print(trunk)
+#             # f.write(trunk)
+# ------------------------------------流式上传(上传大文件)-------------------------------------------------------------
+# with open('big-data', 'rb') as f:
+#     requests.post('http://some.url/streamed', data=f)
+
+# ------------------------------------多文件上传---------------------------------------------------------------------
+# multiple_files = [
+#     ('images', ('foo.png', open('foo.png', 'rb'), 'image/png')),
+#     ('images', ('bar.png', open('bar.png', 'rb'), 'image/png'))
+# ]
+#
+# r = requests.post(host + '/post', files=multiple_files)
+# print(r.text)
+# ------------------------------------hook---------------------------------------------------------------------
+# 目前还是只接收 response 这一个 hook，所以可以通过 hook做一些响应检查或者修改响应的某些信息
+
+
+def callback(response, *args, **kwargs):
+    print(response.url)
+    response.url = 'https://www.taobao.com'
+    response.status_code = 500
+    return response
+
+
+r = requests.get('https://www.baidu.com', hooks=dict(response=callback))
+print(r.url, r.status_code)
+
+# ----------------------------------自定义身份认证 Custom HTTP Auth-----------------------------------------------------
+# https://requests.readthedocs.io/zh_CN/latest/user/advanced.html#custom-auth
+"""
+:param auth: (optional) Auth tuple or callable to enable
+
+            Basic/Digest/Custom HTTP Auth.
+"""
+
+
+class PizzaAuth(AuthBase):
+    """Attaches HTTP Pizza Authentication to the given Request object."""
+    def __init__(self, username):
+        # setup any auth-related data here
+        self.username = username
+
+    def __call__(self, r):
+        # modify and return the request
+        r.headers['X-Pizza'] = self.username
+        return r
+
+
+# requests.get('http://pizzabin.org/admin', auth=PizzaAuth('kenneth'))
+
+# ----------------------------------流式API-----------------------------------------------------
+# https://requests.readthedocs.io/zh_CN/latest/user/advanced.html#streaming-requests
+# TODO 需要一个场景
+# 处理方式：通过r.iter_lines() 和 r.iter_content() 进行处理
+# ----------------------------------代理-----------------------------------------------------
+# https://requests.readthedocs.io/zh_CN/latest/user/advanced.html#proxies
+# socks代理 https://requests.readthedocs.io/zh_CN/latest/user/advanced.html#socks
+# TODO 需要一个场景
+
+# proxies = {
+#     'http': 'http://192.168.0.105:8888',
+#     'https': 'https://192.168.0.105:8888'
+# }
+#
+# r = requests.get('https://baidu.com', proxies=proxies)
+
+# ----------------------------------响应体解码----------------------------------------------------
+# https://requests.readthedocs.io/zh_CN/latest/user/advanced.html#id16
+# 1、响应头里推测编码方式 2、charade库尝试进行解码 如果还是解码失败，可以通过手动指定 r.encoding 来指定解码方式或者
+# 使用 r.content 原始字节
+
+r = requests.options(host+'/cookies')
+print(r.status_code)
+print(r.headers.get('allow'))
+
+# ----------------------------------支持所有的http请求动词，甚至支持定制动词--------------------------------
+# https://requests.readthedocs.io/zh_CN/latest/user/advanced.html#id17
+
+# --------------------------------响应头链接字段-------------------------------------------------------
+# https://requests.readthedocs.io/zh_CN/latest/user/advanced.html#link-headers
+
+# ----------------------------------传输适配器-------------------------------------------------------
+# https://requests.readthedocs.io/zh_CN/latest/user/advanced.html#transport-adapters
+
+# ----------------------------------requests是阻塞式的-------------------------------------------------
+# 基于requests的异步框架：
+#   grequests https://github.com/kennethreitz/grequests
+#   requests-future https://github.com/ross/requests-futures
+
+# ---------------------------------- header 排序----------------------------------------------------
+# https://requests.readthedocs.io/zh_CN/latest/user/advanced.html#header
+
+# ----------------------------------timeout的再次说明-------------------------------------------------
+# requests的里的timeout， 有两个：连接超时；读取超时
+# 连接超时指的是客户端和服务端创建连接，request要等多长时间，即 socket，connect()
+# 读取超时是服务端给客户端发送第一个字节的时候，request要等多场时间，即 socket，read()
+# timeout = 5 ： 会用作 connect 和 read 超时时间
+# timeout = (3.05, 10)  3.05: connect, 10: read
+
+
+# ===================================身份认证==========================================================
+# https://requests.readthedocs.io/zh_CN/latest/user/authentication.html
+# 1、HTTP Basic Auth
+from requests.auth import HTTPBasicAuth
+
+r = requests.get('https://api.github.com/user')
+print(r.status_code)  # 401
+r_authed = requests.get('https://api.github.com/user', auth=HTTPBasicAuth('username', 'password'))
+# 上面的auth参数可以直接简写成auth = ('username', 'password')
+
+# 2、HTTPDigestAuth
+from requests.auth import HTTPDigestAuth
+url = 'http://httpbin.org/digest-auth/auth/user/pass'
+requests.get(url, auth=HTTPDigestAuth('user', 'pass'))
+# 3、OAuth1 和 OAuth2 的认证
+# requests-oauthlib库进行处理
+from requests_oauthlib import OAuth1
+url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
+auth = OAuth1('YOUR_APP_KEY', 'YOUR_APP_SECRET',
+  'USER_OAUTH_TOKEN', 'USER_OAUTH_TOKEN_SECRET')
+
+# 4、以上都不行，定制认证方式，继承 BaseAuth， 在定制的认证类的__call__里实现认证的逻辑，可以参见之前的示例
+
+
+
